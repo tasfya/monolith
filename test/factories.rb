8fd92710c10e47402c17b7dda50fb542a9dea652
abdokin <bashbin.sh@gmail.com>
2025-04-16 15:05:22 +0100

    Add book factory

--- test/factories.rb
+++ test/factories.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
+  factory :book do
+    title { Faker::Book.title }
+    desc { Faker::Book.author }
+    cover { Rack::Test::UploadedFile.new(Rails.root.join("app/assets/images/logo.png"), "image/png") }
+  end
+
   factory :category do
     title { Faker::Lorem.sentence(word_count: 3, supplemental: true, locale: :ar) }
   end
