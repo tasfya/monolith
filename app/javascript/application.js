ba5613fd953b1b06f5135fa592e60739a665c8c9
abdokin <bashbin.sh@gmail.com>
2025-04-17 21:17:33 +0100

    move Glide initialization to application controller for better structure

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,21 +3,5 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-import Glide from '@glidejs/glide'
 
-new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    autoplay: 2000,
-    hoverpause: true,
-    perView: 4,
-    breakpoints: {
-        1024: {
-            perView: 3
-        },
-        768: {
-            perView: 1
-        }
-    }
-}).mount()
 
--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,6 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
+import Glide from '@glidejs/glide'
 
 export default class extends Controller {
+  connect() {
+    new Glide('.glide', {
+      type: 'carousel',
+      startAt: 0,
+      autoplay: 2000,
+      hoverpause: true,
+      perView: 4,
+      breakpoints: {
+        1024: {
+          perView: 3
+        },
+        768: {
+          perView: 1
+        }
+      }
+    }).mount()
+  }
+  
   preventDefault(event) {
     event.preventDefault()
   }
