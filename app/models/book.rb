140d24cb4097c15fc06f0cbc4c8c953aee2da426
abdokin <bashbin.sh@gmail.com>
2025-04-18 12:27:14 +0100

    add category associations and methods to Book model for improved categorization

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,17 @@
 class Book < ApplicationRecord
   has_one_attached :cover
   has_one_attached :file
+
+  has_many :categorizations, as: :categorizable, dependent: :destroy
+  has_many :categories, through: :categorizations
+  
+  default_scope { order(created_at: :desc) }
+  
+  def add_to_category(category)
+    categories << category unless categories.include?(category)
+  end
+  
+  def remove_from_category(category)
+    categories.delete(category)
+  end
 end
